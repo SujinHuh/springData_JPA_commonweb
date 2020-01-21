7af930546b2d4dd18948fbbb7c2aff3cc148ed3a
SujinHuh <juin483@naver.com>
2020-01-22 00:42:44 +0900

    29. 스프링 데이터 Common: Web 4부: HATEOAS - 1
    Page를 PagedResource로 변환하기
      ● 일단 HATEOAS 의존성 추가 (starter-hateoas)
      ● 핸들러 매개변수로 PagedResourcesAssembler
    
    * Test 실패 Jason 형태로 해놓지 않아서

--- src/main/java/me/whiteship/commonweb/post/PostController.java
+++ src/main/java/me/whiteship/commonweb/post/PostController.java
@@ -4,6 +4,9 @@ import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PagedResourcesAssembler;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.PagedModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +20,13 @@ public class PostController {
     PostRepository post;
 
     @GetMapping("/posts/{id}")
-    public String getPost(@PathVariable("id") Post post){
+    public String getPost(@PathVariable("id") Post post) {
         return post.getTitle();
+    }
+
+    @GetMapping("/posts")
+    public PagedModel<EntityModel<Post>> getPosts(Pageable pageable, PagedResourcesAssembler<Post> assembler) {
+        return assembler.toModel(post.findAll(pageable));
     }
 
     @GetMapping("/posts")
--- src/test/java/me/whiteship/commonweb/post/PostControllerTest.java
+++ src/test/java/me/whiteship/commonweb/post/PostControllerTest.java
@@ -48,13 +48,11 @@ public class PostControllerTest {
     @Test
     public void getPosts() throws Exception {
 
-        Post post = new Post();
-        post.setTitle("jpa");
-        postRepository.save(post);
+        createPosts();
 
         mockMvc.perform(get("/posts/")
-                .param("page", "0")
-                .param("size", "0")
+                .param("page", "3")
+                .param("size", "10")
                 .param("sort", "created,desc")
                 .param("sort", "title"))
                 .andDo(print())
@@ -62,5 +60,16 @@ public class PostControllerTest {
                 .andExpect(jsonPath("$.content[0].title", is("jpa")))
         ;
 
+    }
+
+    private void createPosts() {
+
+        int postCount = 100;
+        while (postCount > 0){
+            Post post = new Post();
+            post.setTitle("jpa");
+            postRepository.save(post);
+            postCount--;
+        }
     }
 }
