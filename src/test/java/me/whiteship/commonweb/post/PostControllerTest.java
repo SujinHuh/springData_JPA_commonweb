c0a668b21526a3fbbfa0e899c8e88fe35a175e56
SujinHuh <juin483@naver.com>
2020-01-21 22:41:52 +0900

    스프링 데이터 Common: Web 2부: DomainClassConverter - 1

---
+++ src/test/java/me/whiteship/commonweb/post/PostControllerTest.java
@@ -0,0 +1,45 @@
+package me.whiteship.commonweb.post;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+//Application의 모든 bean이 등록이 되는 인티그레이션 테스트
+public class PostControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Test
+    public void getPost() throws Exception {
+
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+        mockMvc.perform(get("/posts/" + post.getId()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string("jpa"))
+        ;
+
+
+    }
+}
