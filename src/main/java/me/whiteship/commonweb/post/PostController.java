7501e1631b3a05af638e595ef6b487faeb42cd8b
SujinHuh <juin483@naver.com>
2020-01-21 23:51:54 +0900

    28. 스프링 데이터 Common: Web 3부: Pageable과 Sort 매개변수 - 1
    스프링 MVC HandlerMethodArgumentResolver
      ● 스프링MVC 핸들러 메소드의 매개변수로 받을 수 있는 객체를 확장하고 싶을 때 사용하는 인터페이스
      ● https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/web/meth od/support/HandlerMethodArgumentResolver.html
    
    페이징과 정렬 관련 매개변수
     ● page: 0부터 시작.
     ● size: 기본값 20.
     ● sort: property,property(,ASC|DESC)
     ● 예) sort=created,desc&sort=title (asc가 기본값)
    
     *Test code 작성

--- src/main/java/me/whiteship/commonweb/post/PostController.java
+++ src/main/java/me/whiteship/commonweb/post/PostController.java
@@ -2,6 +2,8 @@ package me.whiteship.commonweb.post;
 
 import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +14,16 @@ import java.util.Optional;
 public class PostController {
 
     @Autowired
-    PostRepository postRepository;
+    PostRepository post;
 
     @GetMapping("/posts/{id}")
     public String getPost(@PathVariable("id") Post post){
         return post.getTitle();
+    }
 
+    @GetMapping("/posts")
+    public Page<Post> getPosts(Pageable pageable) {
+
+        return post.findAll(pageable);
     }
 }
--- src/test/java/me/whiteship/commonweb/post/PostControllerTest.java
+++ src/test/java/me/whiteship/commonweb/post/PostControllerTest.java
@@ -10,11 +10,12 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.asyncDispatch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,12 +35,32 @@ public class PostControllerTest {
         Post post = new Post();
         post.setTitle("jpa");
         postRepository.save(post);
+
         mockMvc.perform(get("/posts/" + post.getId()))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().string("jpa"))
         ;
 
+
+    }
+
+    @Test
+    public void getPosts() throws Exception {
+
+        Post post = new Post();
+        post.setTitle("jpa");
+        postRepository.save(post);
+
+        mockMvc.perform(get("/posts/")
+                .param("page", "0")
+                .param("size", "0")
+                .param("sort", "created,desc")
+                .param("sort", "title"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].title", is("jpa")))
+        ;
 
     }
 }
